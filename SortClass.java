2c17e4b7947f0c9eb8008dfb145720e2f257e8e3
Jack <jackbrashier@Jacks-MacBook-Air.local>
2017-04-25 21:52:50 -0400

    Initial Commit

---
+++ SortClass.java
@@ -0,0 +1,19 @@
+/**
+ * Created by jackbrashier on 4/25/17.
+ */
+public class SortClass {
+    public static void swap(int oneNum, int otherNum, int [] numArr) {
+        int saveIdx = numArr[oneNum];
+        numArr[oneNum] = numArr[otherNum];
+        numArr[otherNum] = saveIdx;
+    }
+    public static void sort(int [] numArr) {
+        for(int i = 0; i < numArr.length; i++) {
+            for(int x = i; x > 0; x--) {
+                if(numArr[x] < numArr[x-1]) {
+                    SortClass.swap(x, x-1, numArr);
+                }
+            }
+        }
+    }
+}
---
+++ SortDriver.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+/**
+ * Created by jackbrashier on 4/25/17.
+ */
+public class SortDriver {
+    public static void main(String [] args) {
+        int [] numArr = new int[10];
+        int numCount = 1;
+        for(int i = 0; i < 10; i++) {
+            numArr[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Input number " + numCount + ":"));
+        numCount++;
+        }
+        SortClass.sort(numArr);
+        System.out.println("Sorted Array:\n");
+        for(int x = 0; x < numArr.length; x++) {
+            System.out.print(numArr[x] + ", ");
+        }
+    }
+}
